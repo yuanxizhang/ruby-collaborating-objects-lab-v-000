87d6b194c6b8c98b12704792a75304540353fadc
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-14 16:23:12 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,8 +14,8 @@ class MP3Importer
   end
   
   def import
-    self.files.each do |fname|
-      Song.new_by_filename(fname) 
+    self.files.each do |file_name|
+      Song.new_by_filename(file_name) 
     end
   end 
 end 
