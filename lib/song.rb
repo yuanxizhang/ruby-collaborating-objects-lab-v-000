65a791ec8296764aa3fc29b57a8487209425f86b
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-14 16:14:40 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+#The Song class will be responsible for creating songs given each filename and sending the artist's name (a string) to the Artist class
+
+class Song 
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  
+  #a method that takes in the name and gets the artist object.
+  def artist_name=(name)
+    Artist.find_or_create_by_name(name)
+  end
+  
+  def self.new_by_filename(file) 
+    # parse the filename to find the song name and artist name
+    f_name = file.split(".").first 
+    artist_name = f_name.split(" - ")[0].strip
+    song_name = f_name.split(" - ")[1].strip
+    
+    # create a new song instance 
+    song = self.new(song_name)
+    song.artist = song.artist_name=(artist_name)
+    
+    song
+  end
+end 
