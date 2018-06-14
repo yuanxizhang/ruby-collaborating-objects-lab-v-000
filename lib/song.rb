fea7e38fe82d017016fa30ca02678f377acef0e2
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-14 16:26:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
     
     # create a new song instance 
     new_song = self.new(song_name)
-    new_song.artist = Artist.
+    new_song.artist = Artist.find_or_create_by_name(artist)
     
     song
   end
