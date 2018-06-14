d3b18e7976cde241ca006f0b2fe9ee4fbeaad8cb
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-14 16:41:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
   
   def self.find_or_create_by_name(name) 
     if self.all.find {|a| a.name == name} == nil
-        new_artist = Artist.new(name)
-        new_artist.save
+        artist = Artist.new(name)
+        artist.save
     else
         artist = self.all.find {|a| a.name == name}
     end
